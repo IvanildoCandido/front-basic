5067be8c71fbd4c1fd7d83f772b0184ea26405c4
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-09-23 22:35:25 -0300

    Requisiçao e apresentaçao dos dados

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Código que será executado quando o navegador carregar
+document.addEventListener('DOMContentLoaded', function () {
+  const data = [];
+  fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic')
+    .then((response) => response.json())
+    .then((result) => createImage(result));
 });
+
+const createImage = (data) => {
+  const container = document.querySelector('.feed');
+  data.map(
+    (item) =>
+      (container.innerHTML += `<img class=".img-fluid" src="${item.imagens.thumbnail.url}">`),
+  );
+};
