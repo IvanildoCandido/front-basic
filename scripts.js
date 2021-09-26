d0a106f1b9d43ff500c83c612e54355d3f1fddd9
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-09-25 21:02:30 -0300

    Funçoes de esconder e mostrar dados

--- scripts.js
+++ scripts.js
@@ -2,13 +2,42 @@ document.addEventListener('DOMContentLoaded', function () {
   const data = [];
   fetch('https://us-central1-squid-apis.cloudfunctions.net/test-front-basic')
     .then((response) => response.json())
-    .then((result) => createImage(result));
+    .then((result) => createImage(result))
+    .then(() => showInfo());
 });
 
 const createImage = (data) => {
   const container = document.querySelector('.feed');
   data.map(
     (item) =>
-      (container.innerHTML += `<img class=".img-fluid" src="${item.imagens.thumbnail.url}">`),
+      (container.innerHTML += `
+      <a href="${item.link}" target="_blank" class="feed-image" style="background-image: url(${
+        item.imagens.thumbnail.url
+      });">
+        <div class="info-user">
+          <span>@${item.usuario.username}</span>
+          <i class="fas fa-heart">${' ' + item.upvotes}</i>
+          <i class="fas fa-comment">${' ' + item.comentarios}</i>
+          <i class="fas fa-calendar-alt">${
+            ' ' + moment(item.criadoEm).locale('pt-br').format('l LT')
+          }</i>
+        </div>
+      </a>
+      `),
+  );
+};
+
+const showInfo = () => {
+  const images = document.querySelectorAll('.feed-image');
+  console.log(images);
+  images.forEach((image) =>
+    image.addEventListener('mouseover', (e) => {
+      e.target.querySelector('.info-user').style.display = 'flex';
+    }),
+  );
+  images.forEach((image) =>
+    image.addEventListener('mouseleave', (e) => {
+      e.target.querySelector('.info-user').style.display = 'none';
+    }),
   );
 };
